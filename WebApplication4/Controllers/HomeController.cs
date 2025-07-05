600a10e24bc0343d14b1d0abfd105b61191e1097
DouaaDaoudi <daoudi_douaa@hotmail.com>
2025-07-05 11:00:21 -0400

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class HomeController : ControllerBase
+    {
+        
+        [HttpGet(Name = "GetInt")]
+        public int Get(int id)
+            { 
+            return id; 
+        }
+    }
+}
---
+++ WebApplication4/NewFolder/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.NewFolder
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Invoice> Invoices { get; set; } = null!;
+    }
+}
